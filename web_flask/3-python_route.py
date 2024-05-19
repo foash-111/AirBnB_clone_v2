949dcb9208ae1fecac16b94a94b77af7fafdce0c
foash-111 <mr2221@fayoum.edu.eg>
2024-05-19 20:31:21 +0300

    set origin in my own forked repo

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""my first module"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """hello world from the root"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbhb():
+    """hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text=""):
+    """hbnb"""
+    text = text.replace('_', ' ')
+    return ('C ' + text)
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    """hbnb"""
+    text = text.replace('_', ' ')
+    return ('Python ' + text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    # debug=True, which will be beneficial if you plan to make changes
+    # and want the server to automatically reload
+    # and provide helpful debugging information.
