3c0d3787869ddce6c58ea596d8f5b0f642645ca7
foash-111 <mr2221@fayoum.edu.eg>
2024-05-19 20:54:06 +0300

    set origin in my own forked repo

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -45,7 +45,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """hbnb"""
-    return (render_template('5-number.html', n))
+    return (render_template('5-number.html', n=n))
 
 
 if __name__ == '__main__':
